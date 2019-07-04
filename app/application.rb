485ae640425204ca0fe15f9fc15459adbb311736
Alan Krajina <alankrajina88@gmail.com>
2019-07-04 10:43:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       item = @@item.find{|s| s.title == items_title}
  
       resp.write item.price
-    elsif @@item == nil
+    elsif @@items == nil
     
       resp.status = 400
      else
